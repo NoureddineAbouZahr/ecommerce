8588cc986303bcf79f757bee8273fc5de3890aee
nour az <naz.199860@gmail.com>
2022-06-10 22:33:58 +0300

    favorites controller, add and remove favorites

--- laravel-server/jwt/app/Http/Controllers/FavoriteController.php
+++ laravel-server/jwt/app/Http/Controllers/FavoriteController.php
@@ -4,7 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\DB;
+
 class FavoriteController extends Controller
 {
-    //
+    public function addFav(Request $request){
+        $Fav = new Favorite;
+        $Fav->u_id = $request->uid;
+        $Fav->item_id = $request->item_id;
+        $Fav->save();
+        
+        return response()->json([
+            "status" => "Success"
+        ], 200);
+    }
+
+
+
+    public function remove_favorite(Request $request){
+        $deleted = DB::table('favorites')->where('u_id','=',$request->uid)      
+                                         ->where('item_id','=',$request->item_id);
+        return response()->json([
+            "status" => "Success"
+        ], 200);
+    }
 }
